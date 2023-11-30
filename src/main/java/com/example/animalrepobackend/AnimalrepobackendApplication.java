0b321fbb4281b401eda089ae475c6f868be6cff0
SAAYAN-SAMANTA <saayansamanta2003@gmail.com>
2023-11-30 21:18:26 +0530

    updated

---
+++ src/main/java/com/example/animalrepobackend/AnimalrepobackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalrepobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalrepobackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalrepobackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/animalrepobackend/Entities/Animals.java
@@ -0,0 +1,4 @@
+package com.example.animalrepobackend.Entities;
+
+public class Animals {
+}
